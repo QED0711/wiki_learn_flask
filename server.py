8aff2990f5efadfeacf8953f22edaecbbe60af0a
Quinn Dizon <qdizon@gmail.com>
2019-09-04 21:10:48 -0400

    added stream_with_context

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 
 import flask
-from flask import Flask, request, Response
+from flask import Flask, request, Response, stream_with_context
 from flask_cors import CORS
 
 import sys
@@ -48,58 +48,58 @@ CORS(application)
 
 @application.route('/', methods=["GET", "POST"])
 def predict():
-    # if request.method == "GET":
-    #     return "Hello World"
+    if request.method == "GET":
+        return "Hello World"
 
-    # if request.method == "POST":
-    entry = request.json['entry']
-    def generator(entry):
-        # yield "something here"
+    if request.method == "POST":
+        entry = request.json['entry']
+        def generator(entry):
+            # yield "something here"
 
-        # yield entry
+            # yield entry
 
-        gc = GraphCreator(entry)
+            gc = GraphCreator(entry)
+            
+            # yield f"Layer 1 nodes: {len(gc.graph.nodes)}\n"
+            yield str({"layer 1 nodes":len(gc.graph.nodes)}) + "\n"
+
+            # if len(gc.graph.nodes) > 500:
+            #     return "Too Large"
+
+            rec = Recommender(gc, threads=50, chunk_size=1)
+            yield "Recommender Initialized\n"
+
+            # rec.fit(scaler=Normalizer)
+            # yield "recommend fit\n"
+
+            rec._expand_network()
+            print("expanding\n")
+            yield "expanding\n"
+            rec._graph_cleanup()
+            print("cleanup\n")
+            yield "cleanup\n"
+            rec._get_features()
+            print("features\n")
+            yield "features\n"
+            rec._calculate_similarity()
+            print("similarity\n")
+            yield "similarity\n"
+            rec.scaled = rec._scale_features(Normalizer)
+            print("scaled\n")
+            yield "scaled\n"
+            
+            rec.predict(rf_v2_classifier)
+            yield "predictions complete\n"
+
+            results = rec.format_results(decision_threshold=0.5)
+            yield "Results formatted\n"
+
+            results['classes'] = list(rec.classes)
+            yield "Classes added\n"
+
+            yield str(results)
         
-        # yield f"Layer 1 nodes: {len(gc.graph.nodes)}\n"
-        yield str({"layer 1 nodes":len(gc.graph.nodes)}) + "\n"
-
-        # if len(gc.graph.nodes) > 500:
-        #     return "Too Large"
-
-        rec = Recommender(gc, threads=50, chunk_size=1)
-        yield "Recommender Initialized\n"
-
-        # rec.fit(scaler=Normalizer)
-        # yield "recommend fit\n"
-
-        rec._expand_network()
-        print("expanding\n")
-        yield "expanding\n"
-        rec._graph_cleanup()
-        print("cleanup\n")
-        yield "cleanup\n"
-        rec._get_features()
-        print("features\n")
-        yield "features\n"
-        rec._calculate_similarity()
-        print("similarity\n")
-        yield "similarity\n"
-        rec.scaled = rec._scale_features(Normalizer)
-        print("scaled\n")
-        yield "scaled\n"
-        
-        rec.predict(rf_v2_classifier)
-        yield "predictions complete\n"
-
-        results = rec.format_results(decision_threshold=0.5)
-        yield "Results formatted\n"
-
-        results['classes'] = list(rec.classes)
-        yield "Classes added\n"
-
-        yield str(results)
-    
-    return Response(generator(entry))
+        return Response(stream_with_context(generator(entry)))
 
 
 @application.route("/test", methods=["POST"])
